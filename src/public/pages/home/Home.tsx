03d3842b4524bc41f9c49744ca200bcd2db9438e
JDu202012207 <u202012207@upc.edu.pe>
2024-11-15 10:44:52 -0500

    refactor: update images source and extension.

--- src/public/pages/home/Home.tsx
+++ src/public/pages/home/Home.tsx
@@ -34,18 +34,18 @@ export const Home = () => {
             <h1 className="text-2xl font-semibold ">Entidades Financieras</h1>
           </div>
 
-          {/* Contenedor de imágenes de logos de bancos */}
+          {/* Contenedor de imagenes de logos de bancos */}
           <div className="grid grid-cols-3 gap-4 ml-32 mt-16 mb-16">
-            <img src="https://1000marcas.net/wp-content/uploads/2019/12/BBVA-logo.png" alt="BBVA" className="h-32 object-contain"/>
-            <img src="https://logovtor.com/wp-content/uploads/2020/03/banco-de-credito-del-peru-bcp-logo-vector.png" alt="BCP" className="h-32 object-contain"/>
-            <img src="https://www.bn.com.pe/img/logobn-compartir.png" alt="Banco de la Nación" className="h-32 object-contain"/>
-            <img src="https://logos-world.net/wp-content/uploads/2021/03/Scotiabank-Logo-1998-2019-700x394.png" alt="Scotiabank" className="h-32 object-contain"/>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA43gL7kC104vLhCr-IqwFUZifyejXNlwHoQ&s" alt="Banco Falabella" className="h-32 object-contain"/>
-            <img src="https://emtecgroup.net/wp-content/uploads/2024/01/Banco-Ripley.png" alt="Banco Ripley" className="h-32 object-contain"/>
-            <img src="https://brandemia.org/sites/default/files/inline/images/logo_banco_pichincha_portada.jpg" alt="Banco Pichincha" className="h-32 object-contain"/>
-            <img src="https://logowik.com/content/uploads/images/interbank4094.logowik.com.webp" alt="Interbank" className="h-32 object-contain"/>
-            <img src="https://mir-s3-cdn-cf.behance.net/projects/404/2aa9ad208811221.Y3JvcCwxMDIyLDc5OSwwLDA.jpg" alt="BanBif" className="h-32 object-contain"/>
-            <img src="https://seeklogo.com/images/M/mibanco-logo-699A2E1B3E-seeklogo.com.png" alt="Mibanco" className="h-32 object-contain"/>
+            <img src="/src/assets/home/bbva-logo.jpg" alt="BBVA" className="h-32 object-contain"/>
+            <img src="/src/assets/home/bcp.jpg" alt="BCP" className="h-32 object-contain"/>
+            <img src="/src/assets/home/banco-nacion.jpg" alt="Banco de la Nación" className="h-32 object-contain"/>
+            <img src="/src/assets/home/scotiabank.jpg" alt="Scotiabank" className="h-32 object-contain"/>
+            <img src="/src/assets/home/banco-fallabela.jpg" alt="Banco Falabella" className="h-32 object-contain"/>
+            <img src="/src/assets/home/banco-ripley.jpg" alt="Banco Ripley" className="h-32 object-contain"/>
+            <img src="/src/assets/home/banco-pichincha.jpg" alt="Banco Pichincha" className="h-32 object-contain"/>
+            <img src="/src/assets/home/interbank.jpg" alt="Interbank" className="h-32 object-contain"/>
+            <img src="/src/assets/home/banbif.jpg" alt="BanBif" className="h-32 object-contain"/>
+            <img src="/src/assets/home/mibanco.jpg" alt="Mibanco" className="h-32 object-contain"/>
           </div>
           
           <div className="flex flex-col items-center justify-center bg-gray-300 p-6 rounded-lg">
